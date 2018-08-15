88e91b480f56a60b9950406125e3e19925d6ad8a
aranza <aranza@michelada.io>
2018-08-14 22:47:40 -0500

    Add create method and route

--- app/controllers/group_chat_controller.rb
+++ app/controllers/group_chat_controller.rb
@@ -1,4 +1,19 @@
 class GroupChatController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
+  end
+
+  def create
+    puts params[:message]
+    @message = Message.new(user_uid: session.id, username: params[:username], message: params[:message])
+    @message.save
+    render :index
+  end
+
+  private
+
+  def permited_params
+    params.require(:group_chat).permit(:username, :message)
   end
 end
