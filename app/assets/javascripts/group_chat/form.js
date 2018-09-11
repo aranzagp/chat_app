077784d19c157d95748b55252156f87e65b68cd4
aranza <aranza@michelada.io>
2018-09-11 01:15:11 -0500

    Fix form

--- app/assets/javascripts/group_chat/form.js
+++ app/assets/javascripts/group_chat/form.js
@@ -4,19 +4,17 @@
   const message = document.getElementById('message');
   const sendMessage = document.getElementById('sendMessage');
 
-  console.log(chatForm);
-
   const sendRequest = () => {
     const username = message.dataset.username;
     const data = {username: username, message: message.value};
-    console.log('Send');
     $.ajax({
       url: "group_chat/",
       type: 'POST',
       data: JSON.stringify(data),
       contentType: 'application/json',
     }).done(function(resp) {
-     alert('DONE!');
+     console.log('DONE!');
+     message.value = ''
     }).fail(function(err) {
      console.log("Error", err);
     });
@@ -24,22 +22,7 @@
 
   if(sendMessage){
     sendMessage.addEventListener('click', ev => {
-      console.log('HERE')
       sendRequest();
     });
   };
 })();
-
-// window.clickSubmit = function() {
-//   console.log('HERE');
-  // return $.ajax({
-  //   url: "expenses/" + id,
-  //   type: 'DELETE'
-  // }).done(function(resp) {
-  //   var elem;
-  //   elem = document.getElementById("tr_" + id);
-  //   return elem.parentNode.removeChild(elem);
-  // }).fail(function(err) {
-  //   return console.log("Error", err);
-  // });
-// };
