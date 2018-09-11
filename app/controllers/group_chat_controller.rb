48880d43015277855fc0cdee607f91e8855a064c
aranza <aranza@michelada.io>
2018-09-11 01:21:32 -0500

    Add pusher configuration, rabbitmq consumer and procfile

--- app/controllers/group_chat_controller.rb
+++ app/controllers/group_chat_controller.rb
@@ -2,18 +2,31 @@ class GroupChatController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    @messages = Message.sorted
   end
 
   def create
-    puts params[:message]
-    @message = Message.new(user_uid: session.id, username: params[:username], message: params[:message])
-    @message.save
-    render :index
+    message = Message.create(user_uid: session.id, username: username, message: params[:message])
+    publish(message)
+    render json: message, status: :created
   end
 
   private
 
+  def username
+    session[:username] ||= "guest#{rand(1000)}"
+  end
+
   def permited_params
-    params.require(:group_chat).permit(:username, :message)
+    params.require(:group_chat).permit(:message)
+  end
+
+  def publish(message)
+    connection = Bunny.new
+    connection.start
+    channel = connection.create_channel
+    exchange = channel.fanout('logs')
+    exchange.publish(message.id.to_s)
+    connection.close
   end
 end
